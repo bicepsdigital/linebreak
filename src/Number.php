99f10cc91282bd57e4a4351d05e376826e8e8387
Lysek <lysek.tomas@gmail.com>
2017-12-17 12:30:07 +0100

    fixed numbers with comma

--- src/Number.php
+++ src/Number.php
@@ -1,29 +1,44 @@
 <?php
 
-
 namespace BicepsDigital\LineBreak;
 
-
 class Number extends Word {
-    const CODE = 'number';
 
-    public $type = self::CODE;
+	const CODE = 'number';
 
-    public function isNonBreakableWith(Word $nextWord = null) {
-        return true;
-    }
+	public $type = self::CODE;
 
-    /**
-     * @param $text
-     * @return mixed
-     */
-    protected static function stripCommonlyUsedCharacters($text) {
-        $stringsToRemove = array('(', ')', '-', '.', '&ndash;', '–');
-        return str_replace($stringsToRemove, '', $text);
-    }
+	/**
+	 * @param $text
+	 *
+	 * @return bool
+	 */
+	protected static function lastCharIsComma( $text ) {
+		return substr( $text, - 1 ) === ',';
+	}
 
-    public static function isType($text) {
-        $text = self::stripCommonlyUsedCharacters($text);
-        return is_numeric($text);
-    }
+	public function isNonBreakableWith( Word $nextWord = null ) {
+		return true;
+	}
+
+	/**
+	 * @param $text
+	 *
+	 * @return mixed
+	 */
+	protected static function stripCommonlyUsedCharacters( $text ) {
+		$stringsToRemove = array( '(', ')', '-', '.', '&ndash;', '–' );
+
+		if ( !self::lastCharIsComma( $text) ) {
+			$stringsToRemove[] = ',';
+		}
+
+		return str_replace( $stringsToRemove, '', $text );
+	}
+
+	public static function isType( $text ) {
+		$text = self::stripCommonlyUsedCharacters( $text );
+
+		return is_numeric( $text );
+	}
 }
---
+++ tests/LineBreakUJCTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: lysek
+ * Date: 13.01.2017
+ * Time: 12:38
+ */
+
+namespace Tests;
+
+use BicepsDigital\LineBreak\LineBreak;
+use BicepsDigital\Test\TestCase;
+// http://prirucka.ujc.cas.cz/?id=880
+class LineBreakUJCTest extends TestCase {
+
+    /**
+     * @param $expected
+     * @param $input
+     */
+    protected function assertLineBreak($expected, $input) {
+        $this->assertEquals($expected, LineBreak::parse($input));
+    }
+
+	public function testPreposition() {
+		$this->assertLineBreak('k&nbsp;mostu, s&nbsp;bratrem, v&nbsp;Plzni, z&nbsp;nádraží', 'k mostu, s bratrem, v Plzni, z nádraží');
+	}
+
+	public function testPrepositionSecond() {
+		$this->assertLineBreak('u&nbsp;babičky, o&nbsp;páté, a&nbsp;Janek, i&nbsp;Ivana', 'u babičky, o páté, a Janek, i Ivana');
+	}
+
+	public function testNumbers() {
+		$this->assertLineBreak( '2&nbsp;500, 1&nbsp;000&nbsp;000, 25,325&nbsp;23', '2 500, 1 000 000, 25,325 23' );
+	}
+
+
+}
